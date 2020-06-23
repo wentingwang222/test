d3e4fe99d00f680f11f00f0b684bf762f92691dc
wentingwang222 <59840114+wentingwang222@users.noreply.github.com>
2020-06-23 16:30:16 -0400

    Update mltest.R

--- mltest.R
+++ mltest.R
@@ -12,3 +12,25 @@ library(ggplot2)
 
 ggplot(Orange, aes(age, circumference, color = Tree)) +
   geom_line()
+
+Orange %>% 
+  group_by(Tree) %>%
+  summarize(correlation = cor(age, circumference))
+
+ct <- cor.test(Orange$age, Orange$circumference)
+ct
+
+tidy(ct)
+
+nested <- 
+  Orange %>% 
+  nest(data = c(age, circumference))
+
+nested %>% 
+  mutate(test = map(data, ~ cor.test(.x$age, .x$circumference)))
+
+nested %>% 
+  mutate(
+    test = map(data, ~ cor.test(.x$age, .x$circumference)), # S3 list-col
+    tidied = map(test, tidy)
+  )
